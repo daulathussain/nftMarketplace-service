d7430fb58fb380fa8e5832cb8e1d937a94c5abb8
daulathussain <daulathussain92@gmail.com>
2022-08-22 11:26:53 +0530

    first commit

---
+++ components/componentsindex.js
@@ -0,0 +1,7 @@
+import Button from "./Button/Button";
+import Footer from "./Footer/Footer";
+import NavBar from "./NavBar/NavBar";
+import HeroSection from "./HeroSection/HeroSection";
+import Service from "./Service/Service";
+
+export { Button, Footer, NavBar, HeroSection, Service };
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//INTERNAL IMPORT
+import Style from "../styles/index.module.css";
+import { HeroSection, Service } from "../components/componentsindex";
+
+const Home = () => {
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+      <Service />
+    </div>
+  );
+};
+
+export default Home;
